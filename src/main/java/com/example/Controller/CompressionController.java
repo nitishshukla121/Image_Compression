bdbdee7dc92c364f78022823017bfdea4db243fe
Nitish Shukla <nitish121shukla@gmail.com>
2025-09-01 00:22:17 +0530

    another update to yml and apis

--- src/main/java/com/example/Controller/CompressionController.java
+++ src/main/java/com/example/Controller/CompressionController.java
@@ -12,11 +12,12 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+
 @Controller
 public class CompressionController {
 
     private final CompressionService compressionService;
-
+   
     public CompressionController(CompressionService compressionService) {
         this.compressionService = compressionService;
     }
@@ -32,7 +33,6 @@ public class CompressionController {
         return "result";
     }
 
-
     @GetMapping("/download")
     public ResponseEntity<byte[]> downloadImage(@RequestParam("filename") String filename) throws IOException {
         byte[] compressedData = compressionService.getCompressedFile(filename);
@@ -43,4 +43,37 @@ public class CompressionController {
 
         return ResponseEntity.ok().headers(headers).body(compressedData);
     }
+    
+//    @GetMapping("/display")
+//    public ResponseEntity<byte[]> displayImage(@RequestParam("filename") String filename) throws IOException {
+//        Path imagePath = Paths.get(COMPRESSED_IMAGE_PATH + filename);
+//
+//        logger.info("Attempting to load image for display from path: " + imagePath.toAbsolutePath());
+//
+//        try {
+//            if (Files.exists(imagePath) && Files.isReadable(imagePath)) {
+//                byte[] imageBytes = Files.readAllBytes(imagePath);
+//                HttpHeaders headers = new HttpHeaders();
+//
+//                String fileExtension = filename.substring(filename.lastIndexOf(".") + 1).toLowerCase();
+//                if (fileExtension.equals("jpg") || fileExtension.equals("jpeg")) {
+//                    headers.setContentType(MediaType.IMAGE_JPEG);
+//                } else if (fileExtension.equals("png")) {
+//                    headers.setContentType(MediaType.IMAGE_PNG);
+//                }
+//                
+//                headers.setContentLength(imageBytes.length);
+//                return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+//            } else {
+//                logger.error("File not found or not readable: " + imagePath.toAbsolutePath());
+//                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//            }
+//        } catch (NoSuchFileException e) {
+//            logger.error("File not found: " + imagePath.toAbsolutePath());
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        } catch (IOException e) {
+//            logger.error("Error reading file: " + imagePath.toAbsolutePath(), e);
+//            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+    
 }
